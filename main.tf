7a6d7e8793b70a48ad923c98bb25ae026d17b648
swedev99 <swedevops99@gmail.com>
2023-06-01 12:23:23 +0530

    code dry

--- main.tf
+++ main.tf
@@ -1,6 +1,18 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
-  enable_dns_support = true
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
   enable_dns_hostnames = true
+  tags                 = merge(var.tags, { Name = "${var.env}-vpc" })
+}
+
+module "subnets" {
+   source = "./subnets"
+
+  for_each = var.subnets
+  vpc_id = aws_vpc.main.id
+  cidr_block = each.value["cidr_block"]
+  name = each.value["name"]
+
   tags = var.tags
+  env = var.env
 }
