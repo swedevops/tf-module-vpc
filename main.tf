c1dd696814443c463e198353d2be58c9a53a3d24
swedev99 <swedevops99@gmail.com>
2023-06-01 20:02:57 +0530

    code dry

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_eip" "ngw" {
 resource "aws_nat_gateway" "ngw" {
   count = length(var.subnets["public"].cidr_block)
   allocation_id = aws_eip.ngw[count.index].id
-  subnet_id     = module.subnets["public"].subnets_ids[count.index]
+  subnet_id     = module.subnets["public"].subnet_ids[count.index]
 
   tags   = merge(var.tags, { Name = "${var.env}-ngw" })
 
