359b072d36810004aaae4d0c023f9714104d3b13
swedev99 <swedevops99@gmail.com>
2023-06-02 10:42:33 +0530

    code dry

--- subnets/main.tf
+++ subnets/main.tf
@@ -12,8 +12,8 @@ resource "aws_route_table" "main" {
 
   tags =  merge(var.tags, { Name = "${var.env}-${var.name}-rt-${count.index+1}" })
   }
-resource "aws_main_route_table_association" "associate" {
-  count  = length(var.cidr_block)
-  vpc_id         = aws_subnet.main[count.index].id
-  route_table_id = aws_route_table.main[count.index].id
+resource "aws_route_table_association" "associate" {
+  count             = length(var.cidr_block)
+  subnet_id         = aws_subnet.main[count.index].id
+  route_table_id    = aws_route_table.main[count.index].id
 }
