1220eb00f927dc89e8b5f773bd8c81e3c073e0ce
swedev99 <swedevops99@gmail.com>
2023-06-02 15:46:19 +0530

    code dry

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "aws_route" "ngw" {
   count                  = length(local.all_private_subnet_ids)
   route_table_id         = local.all_private_subnet_ids[count.index]
   nat_gateway_id         = element(aws_nat_gateway.ngw.*.id, count.index)
-  destination_cidr_block = "10.0.0.0/0"
+  destination_cidr_block = "0.0.0.0/0"
 }
 
 output "subnets" {
