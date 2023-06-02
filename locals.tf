59984ea5ad5453292de54ee8eba06ac1167c2d6d
swedev99 <swedevops99@gmail.com>
2023-06-02 22:48:40 +0530

    code dry

--- locals.tf
+++ locals.tf
@@ -1,3 +1,3 @@
 locals {
-  all_private_subnet_ids = concat(module.subnets["db"].route_table_ids,module.subnets["app"].route_table_ids,module.subnets["web"].route_table_ids)
+  all_private_subnet_ids = concat(module.subnets["db"].route_table_ids, module.subnets["app"].route_table_ids, module.subnets["web"].route_table_ids)
 }
