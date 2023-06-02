8a7fa74102d07db7b6ed5aa5b93716b7fbfa042b
swedev99 <swedevops99@gmail.com>
2023-06-02 12:59:03 +0530

    code dry

--- main.tf
+++ main.tf
@@ -44,11 +44,14 @@ resource "aws_route" "igw" {
   destination_cidr_block = "0.0.0.0/0"
 }
 #resource "aws_route" "ngw" {
-#  count                  = length(module.subnets["public"].route_table_ids)
-#  route_table_id         = module.subnets["public"].route_table_ids[count.index]
-#  gateway_id             = aws_internet_gateway.igw.id
+#  count                  = length(local.all_private_subnet_ids)
+#  route_table_id         = local.all_private_subnet_ids[count.index]
+#  nat_gateway_id         = aws_nat_gateway.ngw.id
 #  destination_cidr_block = "0.0.0.0/0"
 #}
+output "ngw" {
+  value = aws_nat_gateway.ngw
+}
 
 
 
