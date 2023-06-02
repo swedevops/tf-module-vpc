70c361ed6be89dab7c0da46c31be68e3a8d23d66
swedev99 <swedevops99@gmail.com>
2023-06-02 13:31:55 +0530

    code dry

--- main.tf
+++ main.tf
@@ -37,18 +37,20 @@ resource "aws_nat_gateway" "ngw" {
   tags   = merge(var.tags, { Name = "${var.env}-ngw" })
 
 }
+
 resource "aws_route" "igw" {
   count                  = length(module.subnets["public"].route_table_ids)
   route_table_id         = module.subnets["public"].route_table_ids[count.index]
   gateway_id             = aws_internet_gateway.igw.id
   destination_cidr_block = "0.0.0.0/0"
 }
-#resource "aws_route" "ngw" {
-#  count                  = length(local.all_private_subnet_ids)
-#  route_table_id         = local.all_private_subnet_ids[count.index]
-#  nat_gateway_id         = aws_nat_gateway.ngw.id
-#  destination_cidr_block = "0.0.0.0/0"
-#}
+
+resource "aws_route" "ngw" {
+  count                  = length(local.all_private_subnet_ids)
+  route_table_id         = local.all_private_subnet_ids[count.index]
+  nat_gateway_id         = element(aws_nat_gateway.ngw.*.id, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+}
 output "ngw" {
   value = aws_nat_gateway.ngw
 }
