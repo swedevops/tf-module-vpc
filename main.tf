176f51e1760e278ba2055d0f811b3fa4ba847dbd
swedev99 <swedevops99@gmail.com>
2023-06-04 19:36:40 +0530

    code dry

--- main.tf
+++ main.tf
@@ -48,6 +48,7 @@ resource "aws_route" "ngw" {
 }
 
 resource "aws_vpc_peering_connection" "peer" {
+  peer_owner_id = data.aws_caller_identity.identify.account_id
   peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.main.id
   auto_accept   = true
